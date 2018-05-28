e41b983959c295401181c557a8d93e59dbd41bbd
Joosung Ahn <joosung21@gmail.com>
2018-05-28 12:32:05 +0900

    Done Tport Sellers Page

--- app.js
+++ app.js
@@ -6,12 +6,32 @@ app.set('view engine', 'pug')
 
 app.use('/', express.static('public'))
 app.use('/assets/', express.static('assets'))
+app.use('/images/', express.static('assets/images'))
 app.use('/node_modules/', express.static('node_modules'))
 
-app.get('/', function (req, res){res.render('sales')})
-app.get('/sales', function (req, res){res.render('sales')})
+app.get('/', function (req, res){res.redirect('sales')})
 
-// Error Pages
+// 판매내역
+app.get('/sales', function (req, res){
+  res.render('sales', {page_name: 'sales'})
+})
+
+// 리포트
+app.get('/report', function (req, res){
+  res.render('report', {page_name: 'report'})
+})
+
+// 숙소정산
+app.get('/payment/seller', function (req, res){
+  res.render('payment-seller', {page_name: 'payment-seller'})
+})
+
+// 숙소관리
+app.get('/tport/sellers', function (req, res){
+  res.render('tport-sellers', {page_name: 'tport-sellers'})
+})
+
+// Error Page
 app.get('*', function (req, res){res.render('404')})
 
 app.listen(3500, function () {
