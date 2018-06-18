539c9b1dacccbad86468105b2cd3a05ff83dfd19
Joosung Ahn <joosung21@gmail.com>
2018-06-18 11:12:39 +0900

    Edit  GDS Property

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,3 +2,12 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// GDS Review Textarea Auto Height
+$(document).ready(function() {
+  $('.wrap').on( 'keyup', 'textarea', function (e){
+    $(this).css('height', 'auto' );
+    $(this).height( this.scrollHeight );
+  });
+  $('.wrap').find( 'textarea' ).keyup();
+});
